d76a41f1f425282062dbcb1e214ebe9056e7f7f0
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2022-10-11 20:07:53 +0530

    error corrected

--- Lab01/Task2/KnightRiders/src/main.cpp
+++ Lab01/Task2/KnightRiders/src/main.cpp
@@ -1,41 +1,37 @@
+/*
+ * Lab1 task 2.c
+  Author: Karan Rasathurai
+ */ 
+
 #include <avr/io.h>
+#include <stdio.h>
 #include <util/delay.h>
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 3000
 int main (void){
 //DDRB = DDRB|(1<<5); /* configure pin 5 of PORTB for output*/
 
 unsigned char z;
-DDRB=0xFF;
-PORTB =0x00;
+DDRB=0xFF; // set the portb as output
+PORTB =0x00; // set the portb to low
 while(1){
-//for (int i=0;i<4;i++){
- // PORTB =PORTB | (1<<i);
- // _delay_ms(BLINK_DELAY_MS);
- // PORTB = ~ PORTB |(1<<i);
+for (int i=0;i<4;i++){
+  PORTB =PORTB | (1<<i); //activate a portbi
+  _delay_ms(BLINK_DELAY_MS); //set delay
+  printf("led lights on  ");
+  PORTB =  PORTB &~ (1<<i);//Deactivate a portbi
+  printf("led lights off  ");
     
-//}
+}
 
-//for (int i=3;i>=0;i--){
- // PORTB =PORTB | (1>>i);
- // _delay_ms(BLINK_DELAY_MS);
- // PORTB = ~ PORTB |(1>>i);
+for (int i=3;i>=0;i++){
+  PORTB =PORTB | (1<<i);//activate a portbi
+  _delay_ms(BLINK_DELAY_MS);//set delay
+  PORTB =  PORTB &~(1<<i);//Deactivate a portbi
     
-//}
-   PORTB =0b00000001;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00000010;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00000100;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00001000;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00000100;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00000010;
-     _delay_ms(BLINK_DELAY_MS);
-    PORTB =0b00000001;
-     _delay_ms(BLINK_DELAY_MS);
+}
 
+
+   
 
 
 
